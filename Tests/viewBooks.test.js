77d4805ab18152cdf598736dbb8f5bf706d28471
divypatel-05 <divypatel636@gmail.com>
2024-12-26 10:26:38 +0530

    Test : Try to view books when One book is present

--- Tests/viewBooks.test.js
+++ Tests/viewBooks.test.js
@@ -14,4 +14,22 @@ describe('LibraryManagementSystem - viewAvailableBooks Tests', () => {
         expect(consoleSpy).toHaveBeenCalledWith("Sorry, no books are available in the library.");
         consoleSpy.mockRestore();
     });
+
+    // Test when atleast one book is available
+    test('viewAvailableBooksTest', () => {
+        const consoleSpy = jest.spyOn(console, 'log').mockImplementation();
+
+        // Add a single book
+        lms.addBook({ title: "Ikigai", author: "Héctor García", ISBN: "8989-123-456-789", publicationYear: 2017 });
+
+        lms.viewAvailableBooks();
+        expect(consoleSpy).toHaveBeenCalledWith("Available Books are : \n");
+        expect(consoleSpy).toHaveBeenCalledWith(
+            "Title: Ikigai\n" +
+            "Author: Héctor García\n" +
+            "PublicationYear: 2017\n" +
+            "ISBN: 8989-123-456-789\n"
+        );
+        consoleSpy.mockRestore();
+    });
 });
